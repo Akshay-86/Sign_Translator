1fd5d212bfcd0ad494c9c25f760a5f99b61f3264
Akshay Kumar <nalliakshaykumar@gmail.com>
2025-09-11 17:58:09 +0530

    updates

--- app/src/main/java/com/example/voxignota/TranslationActivity.java
+++ app/src/main/java/com/example/voxignota/TranslationActivity.java
@@ -1,25 +1,58 @@
 package com.example.voxignota;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
-
-
-
-import java.util.HashMap;
-import java.util.Map;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
+import com.google.common.util.concurrent.ListenableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class TranslationActivity extends AppCompatActivity {
+
+    private PreviewView previewView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_translation);
 
+        previewView = findViewById(R.id.cameraPreview);
 
+        ListenableFuture<ProcessCameraProvider> cameraProviderFuture =
+                ProcessCameraProvider.getInstance(this);
+
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+
+                Preview preview = new Preview.Builder().build();
+                preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+                CameraSelector cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA;
+
+                cameraProvider.bindToLifecycle(
+                        TranslationActivity.this,
+                        cameraSelector,
+                        preview
+                );
+
+            } catch (ExecutionException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, ContextCompat.getMainExecutor(this));
+
+
+    }
+
+    public void goHome(View v){
+        startActivity(new Intent(this, HomeActivity.class));
+        finish();
     }
 
 }
